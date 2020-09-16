fd1b6b70d1dbaf3a2608f9e12103a7ceac5486fe
alexismolineau <alexis.molineau@gmail.com>
2020-09-16 11:02:49 +0200

    changes to BlogController to return H1 and removed the @route

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,17 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
 {
-    /**
-     * @Route("/blog", name="blog")
-     */
+
     public function index()
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
+      return new Response('<h1>Hello World!</h1>');
     }
 }
