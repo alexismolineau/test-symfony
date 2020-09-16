5560e6f227b886343d32e42cec2465712bb57633
alexismolineau <alexis.molineau@gmail.com>
2020-09-16 14:05:49 +0200

    added function in BlogController to deal with routes.yalm

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
 {
@@ -12,4 +12,25 @@ class BlogController extends AbstractController
     {
       return $this->render('blog/index.html.twig');
     }
+
+    public function add()
+    {
+      return new Response('<h1>Page d\'ajout d\'article</h1>');
+    }
+
+    public function show($url)
+    {
+      return new Response('<h1>Vous êtes sur la page '. $url .'</h1>');
+    }
+
+    public function edit($id)
+    {
+      return new Response('<h1>Vous êtes dans le menu d\'édition de l\'article '. $id .'</h1>' );
+    }
+
+    public function remove($id)
+    {
+      return new Response('<h1>Voulez-vous effacer l\'article n' . $id . '</h1>');
+    }
+
 }
